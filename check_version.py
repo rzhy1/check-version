25f8f4dbc0827764a213cd60a5b96171a634fc97
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-10-08 23:03:35 +0800

    Update check_version.py

--- check_version.py
+++ check_version.py
@@ -15,7 +15,7 @@ current_versions = {
     "gettext-tools": "0.26",
     "gmp": "6.3.0",
     "gnutls": "3.8.10",
-    "gpg-error": "1.55",
+    "gpg-error": "1.56",
     "gpgme": "2.0.1",
     "isl": "0.27",
     "libassuan": "3.0.2",
