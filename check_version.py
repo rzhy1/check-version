53f2c64caa7ea17d60c5165a1997bd5856f7ddee
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2026-05-18 20:11:01 +0800

    Update check_version.py

--- check_version.py
+++ check_version.py
@@ -96,7 +96,7 @@ current_versions = {
     "gmp": "6.3.0",
     "gnutls": "3.8.13",
     "gpg-error": "1.61",
-    "gpgme": "2.0.1",
+    "gpgme": "2.1.0",
     "isl": "0.27",
     "libassuan": "3.0.2",
     "libiconv": "1.19",
